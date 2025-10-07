d601299778e6966f46e2f45d6d79d624e67e955e
Timothee Romane <romaneeugene89@gmail.com>
2025-10-07 12:38:28 -0400

    Mettre à jour main.swift

--- main.swift
+++ main.swift
@@ -1,118 +1,105 @@
-//Gestion Etudiant
-func ajouterEtudiant(){
-
-}
-func listerEtudiant(){
-
-}
-func CalculerMoyenne(){
-
-}
-//Gestion Economat
-func ajouterTransaction(){
-
-}
-func listerTransaction(){
-
-}
-func calculerSolde(){
-
-}
-//Gestion Menu
 func afficherMenuPrincipal() {
-    print("\n" + "==================================================")
-    print("SYSTÈME DE GESTION SCOLAIRE")
+    print("\n==================================================")
+    print("SYSTEME DE GESTION SCOLAIRE")
     print("==================================================")
-    print("1.Gestion des étudiants")
-    print("2.Gestion de l'économat")
-    print("3.Quitter")
-    print("Choisissez entre 1 a 3: ")
+    print("1. Gestion des etudiants")
+    print("2. Gestion de l'economat")
+    print("3. Quitter")
+    print("Choisissez entre 1 et 3:")
 }
 
 func afficherMenuEtudiants() {
     while true {
-        print("\nGESTION DES ÉTUDIANTS")
-        print("1.Ajouter un étudiant")
-        print("2.Lister les étudiants")
-        print("3.Calculer la moyenne des notes par matiere")
-        print("4.Retour au menu principal")
-        print("Choisissez entre 1 a 4")
-        
-        let choix = "4"
-        print(choix)
-        
-        switch choix {
-        case "1":
-            ajouterEtudiant()
-        case "2":
-            listerEtudiants()
-        case "3":
-            CalculerMoyenne()
-        case "4":
-            break
-        default:
-            print("Option invalide")
-        }
-        
-        if choix == "4" {
-            break
+        print("\nGESTION DES ETUDIANTS")
+        print("1. Ajouter un etudiant")
+        print("2. Lister les etudiants")
+        print("3. Calculer la moyenne des notes par matiere")
+        print("4. Retour au menu principal")
+        print("Choisissez entre 1 et 4:")
+
+        if let choix = readLine() {
+            switch choix {
+            case "1":
+                ajouterEtudiant()
+            case "2":
+                listerEtudiants()
+            case "3":
+                CalculerMoyenne()
+            case "4":
+                return
+            default:
+                print("Option invalide")
+            }
         }
     }
 }
 
 func afficherMenuEconomat() {
     while true {
-        print("\nGESTION DE L'ÉCONOMAT")
-        print("1.Ajouter une transaction")
-        print("2.Lister les transactions")
-        print("3.Voir le solde")
-        print("4.Retour au menu principal")
-        print("Choisissez entre 1 a 4")
-        
-        let choix = "4"
-        print(choix)
-        
-        switch choix {
-        case "1":
-            ajouterTransaction()
-        case "2":
-            listerTransactions()
-        case "3":
-            calculerSolde()
-        case "4":
-            break
-        default:
-            print("Option invalide")
-        }
-        
-        if choix == "4" {
-            break
-        }
-    }
-}
-func main() {
-    while true {
-        afficherMenuPrincipal()
-        
-        let choix = "3"
-        print(choix)
-        
-        switch choix {
-        case "1":
-            afficherMenuEtudiants()
-        case "2":
-            afficherMenuEconomat()
-        case "3":
-            print("\nMerci d'avoir utilisé le système! Au revoir!")
-            break
-        default:
-            print("Option invalide. Choisissez entre 1, 2 ou 3.")
-        }
-        
-        if choix == "3" {
-            break
+        print("\nGESTION DE L'ECONOMAT")
+        print("1. Ajouter une transaction")
+        print("2. Lister les transactions")
+        print("3. Voir le solde")
+        print("4. Retour au menu principal")
+        print("Choisissez entre 1 et 4:")
+
+        if let choix = readLine() {
+            switch choix {
+            case "1":
+                ajouterTransaction()
+            case "2":
+                listerTransactions()
+            case "3":
+                calculerSolde()
+            case "4":
+                return
+            default:
+                print("Option invalide")
+            }
         }
     }
 }
 
+func main() {
+    while true {
+        afficherMenuPrincipal()
+        
+        if let choix = readLine() {
+            switch choix {
+            case "1":
+                afficherMenuEtudiants()
+            case "2":
+                afficherMenuEconomat()
+            case "3":
+                print("\nMerci d'avoir utilise le systeme! Au revoir!")
+                return
+            default:
+                print("Option invalide. Choisissez entre 1, 2 ou 3.")
+            }
+        }
+    }
+}
+
+
+func ajouterEtudiant() { 
+print(" Fonction: ajouterEtudiant() appelee") 
+}
+func listerEtudiants() { 
+print(" Fonction: listerEtudiants() appelee") 
+}
+func CalculerMoyenne() { 
+print(" Fonction: CalculerMoyenne() appelee")
+}
+
+func ajouterTransaction() { 
+print(" Fonction: ajouterTransaction() appelee") 
+}
+func listerTransactions() { 
+print(" Fonction: listerTransactions() appelee") 
+}
+func calculerSolde() {
+print(" Fonction: calculerSolde() appelee") 
+}
+
 main()
+
